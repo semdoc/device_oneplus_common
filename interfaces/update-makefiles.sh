0cd470c4d71d8531e7be88595efbd3d72edfa9a2
Bruno Martins <bgcngm@gmail.com>
2022-03-04 16:38:44 +0000

    interfaces: Split between vendors
    This should make update-makefiles.sh usable.
    
    aicp: get inline with LoS hardware_oneplus
    
    Change-Id: I0e10968292464eac49c6b741bf277201962fdaa3

--- interfaces/update-makefiles.sh
+++ interfaces/update-makefiles.sh
@@ -3,4 +3,7 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.oneplus:device/oneplus/common/interfaces"
+  "vendor.goodix:device/oneplus/common/interfaces/goodix"
+
+do_makefiles_update \
+  "vendor.oneplus:device/oneplus/common/interfaces/oneplus"
